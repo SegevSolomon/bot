5dd2d314c400dae8706c46e0b38e8d76d7b67a10
SegevSolomon <89121546+SegevSolomon@users.noreply.github.com>
2023-12-22 13:49:29 +0200

    Update index copy.js

--- index copy.js
+++ index copy.js
@@ -8,15 +8,13 @@ app.use(express.json());
 app.post("/incomingMessages", async (req, res) => {
   try {
     const incomingMessage = req.body;
-
-    // console.log(incomingMessage);
     const typeWebhook = incomingMessage.typeWebhook;
     if (typeWebhook == "incomingMessageReceived") {
       functions.incomingMessageReceived(incomingMessage);
     } else if (typeWebhook == "stateInstanceChanged") {
       functions.stateInstanceChanged(incomingMessage);
     } else {
-      console.log("its dont working!");
+      console.log("NO ROUTE");
     }
   } catch (error) {
     console.log(error);
